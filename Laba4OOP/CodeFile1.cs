09ded821d9b88e8af61f9e75cbe457328b40b9ec
6882899 <6882899@stud.nau.edu.ua>
2021-11-15 00:01:45 +0300

    Add project files.

---
+++ Laba4OOP/CodeFile1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Laba4OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double max, min, q;
+            Console.Write("Enter the x value: ");
+            double x = double.Parse((Console.ReadLine()));
+            Console.Write("Enter the y value: ");
+            double y = double.Parse((Console.ReadLine()));
+            Console.Write("Enter the z value: ");
+            double z = double.Parse((Console.ReadLine()));
+            if (x > y + z)
+            {
+                min = y + z;
+            }
+            else
+            {
+                min = x;
+            }
+            if (Math.Pow(x, 2) > y)
+            {
+                max = x;
+            }
+            else
+            {
+                max = y;
+            }
+            q = max + Math.Pow(z, 3);
+            if (q == 0)
+            {
+                Console.WriteLine("Помилка");
+            }
+            Console.WriteLine("F = " + min / q);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba4OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Laba4OOP
+{
+    internal class Program1Base
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            float x, y;
+            Console.WriteLine("Enter x:");
+            x = float.Parse((Console.ReadLine()));
+            Console.WriteLine("Enter y:");
+            y = float.Parse((Console.ReadLine()));
+            if (Math.Abs(y) > 12 || Math.Abs(x) > 12)
+            {
+                N = 4;
+            }
+            else
+            {
+                if (x == 0 && y == 0)
+                {
+                    N = 0;
+                }
+                else if (x > 0)
+                {
+                    N = y > 0 ? 2 : 3;
+                }
+                else
+                {
+                    N = y > 0 ? 1 : 2;
+                }
+            }
+            Console.WriteLine("M = (" + x + ";" + y + ") лежить у межах " + N);
+            Console.ReadKey();
+        }
+    }
+}
